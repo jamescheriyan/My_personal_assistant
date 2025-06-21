97c65d109a88ab1b92805e149f4bd8595179f65a
James Cheriyan <63226335+jamescheriyan@users.noreply.github.com>
2025-06-21 19:39:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ st.set_page_config(
     layout="centered"
 )
 st.markdown("""
-<h3>(✧◡✧)<span style="font-size: 2.5rem;">🤖</span> — Hello! I’m <b>JAi</b>, your AI Assistant.</h3>
+<h3><span style="font-size: 5.5rem;">🤖</span> Hello! I’m <b>JAi</b>, your AI Assistant.</h3>
 <h5><i>James’s Mind • AI’s Precision</i></h5>
 """, unsafe_allow_html=True)
 
