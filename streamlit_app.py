92f27fb8dbd0f9bcee2d90b70bc1d866b98d5834
James Cheriyan <63226335+jamescheriyan@users.noreply.github.com>
2025-06-21 21:59:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -230,7 +230,7 @@ st._bottom.text_input(
     "Ask a question about James Cheriyan’s resume:", 
     key="user_input", 
     on_change=send_message,
-    placeholder="Type your question and press Enter...                                                              ➤")
+    placeholder="Type your question and press Enter...                                                                                                          ➤")
  
 
 
